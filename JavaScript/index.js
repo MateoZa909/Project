215302ce6e82dbecab436947aed15b0b1dcf6e29
MateoZa909 <matbo5959@gmail.com>
2023-10-24 19:11:55 -0500

    first commit

---
+++ JavaScript/index.js
@@ -0,0 +1,9 @@
+const lista = document.querySelector('.lista');
+
+const section = document.querySelector('.file-two');
+const btn = document.querySelector('#buttn');
+
+btn.addEventListener('click', () => {
+    section.style.display = 'none'
+})
+
